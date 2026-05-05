f6aa3a7d08dd98688f8c6c5842ce8d0b7050b396
Aya <ayavic355@gmail.com>
2026-05-05 15:21:09 +0200

    update supabase config

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -2,5 +2,4 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://fniiuatohqdagbqhfvyt.supabase.co";
 const supabaseAnonKey = "sb_publishable_DNMgHAv46a9FIKbuFs0aCw_wdmmrPfP";
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
